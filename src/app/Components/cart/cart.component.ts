5f3ac79cb68975ab4ef2337791d3eefe1d53723b
gitDeveloper2 <backupsarea@gmail.com>
2022-03-10 21:48:22 +0300

    second commit

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CartService} from "src/app/Services/cart.service";
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+public products:any=[];
+    public grand_Total !:number;
+  constructor(private cart_Service:CartService) { }
 
   ngOnInit(): void {
+this.cart_Service.getProducts().subscribe(res=>{
+  console.log(res);
+
+  this.products=res;
+  this.grand_Total=this.cart_Service.getTotalPrices();
+  console.log(this.grand_Total);
+
+})
+  }
+
+  removeall(){
+    this.cart_Service.removeall();
+  }
+  remove(product:any){
+    this.cart_Service.remove_From_Cart(product);
   }
 
 }
--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService } from "src/app/Services/api.service";
+import {CartService } from "src/app/Services/cart.service";
+
+
 
 @Component({
   selector: 'app-products',
@@ -8,12 +11,15 @@ import {ApiService } from "src/app/Services/api.service";
 })
 export class ProductsComponent implements OnInit {
 Products: any;
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private Cart_Service:CartService) { }
 
   ngOnInit(): void {
   this.api.getProducts().subscribe(res=>{
     this.Products=res;
   });
+  }
+  addToCart(product:any){
+    this.Cart_Service.add_to_Cart(product);
   }
 
 }
